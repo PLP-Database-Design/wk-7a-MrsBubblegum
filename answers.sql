3011204a00889e98f0661d8d866bb63654b5fcca
Cherozade <144368894+MrsBubblegum@users.noreply.github.com>
2025-05-05 13:13:39 +0200

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -1,61 +1,61 @@
 -- Question 1
--- step 1: Create database
+-- Create database
 CREATE DATABASE productsDB;
--- create table
-USE productsDB;
+USE  productsDB;
+
+-- Create Table
 CREATE TABLE ProductDetail (
-    OrderID INT,
-    CustomerName VARCHAR(100),
-    Products VARCHAR(255)
-);
-
-INSERT INTO ProductDetail (OrderID, CustomerName, Products) VALUES
-(101, 'John Doe', 'Laptop, Mouse'),
-(102, 'Jane Smith', 'Tablet, Keyboard, Mouse'),
-(103, 'Emily Clark', 'Phone');
-
--- Create normalized table: 1NF
-CREATE TABLE ProductDetail_1NF (
     OrderID INT,
     CustomerName VARCHAR(100),
     Product VARCHAR(100)
 );
 
--- Insert atomic values into 1NF table
-INSERT INTO ProductDetail_1NF VALUES (101, 'John Doe', 'Laptop');
-INSERT INTO ProductDetail_1NF VALUES (101, 'John Doe', 'Mouse');
-INSERT INTO ProductDetail_1NF VALUES (102, 'Jane Smith', 'Tablet');
-INSERT INTO ProductDetail_1NF VALUES (102, 'Jane Smith', 'Keyboard');
-INSERT INTO ProductDetail_1NF VALUES (102, 'Jane Smith', 'Mouse');
-INSERT INTO ProductDetail_1NF VALUES (103, 'Emily Clark', 'Phone');
+-- Populating table
+INSERT INTO ProductDetail (OrderID, CustomerName, Product)
+VALUES
+(101, 'John Doe', 'Laptop'),
+(101, 'John Doe', 'Mouse'),
+(102, 'Jane Smith', 'Tablet'),
+(102, 'Jane Smith', 'Keyboard'),
+(102, 'Jane Smith', 'Mouse'),
+(103, 'Emily Clark', 'Phone');
 
--- Quetsion 2
--- Split into 2 tables
--- Table 1: Orders
+-- Viewing table in 1NF
+SELECT * FROM ProductDetail;
+
+
+-- Question 2
+
+USE productsDB;
+-- Create 2 tables
+-- First Table
 CREATE TABLE Orders (
     OrderID INT PRIMARY KEY,
     CustomerName VARCHAR(100)
 );
 
--- Table 2: OrderItems
-CREATE TABLE OrderItems (
+-- Insert data
+INSERT INTO Orders (OrderID, CustomerName)
+VALUES
+(101, 'John Doe'),
+(102, 'Jane Smith'),
+(103, 'Emily Clark');
+
+-- Second Table
+CREATE TABLE OrderDetails (
+    OrderDetailID INT AUTO_INCREMENT PRIMARY KEY,
     OrderID INT,
     Product VARCHAR(100),
     Quantity INT,
-    PRIMARY KEY (OrderID, Product),
     FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
 );
 
--- Insert values
--- Insert into Orders table
-INSERT INTO Orders VALUES (101, 'John Doe');
-INSERT INTO Orders VALUES (102, 'Jane Smith');
-INSERT INTO Orders VALUES (103, 'Emily Clark');
-
--- Insert into OrderItems table
-INSERT INTO OrderItems VALUES (101, 'Laptop', 2);
-INSERT INTO OrderItems VALUES (101, 'Mouse', 1);
-INSERT INTO OrderItems VALUES (102, 'Tablet', 3);
-INSERT INTO OrderItems VALUES (102, 'Keyboard', 1);
-INSERT INTO OrderItems VALUES (102, 'Mouse', 2);
-INSERT INTO OrderItems VALUES (103, 'Phone', 1);
+-- Insert data
+INSERT INTO OrderDetails (OrderID, Product, Quantity)
+VALUES
+(101, 'Laptop', 2),
+(101, 'Mouse', 1),
+(102, 'Tablet', 3),
+(102, 'Keyboard', 1),
+(102, 'Mouse', 2),
+(103, 'Phone', 1);
